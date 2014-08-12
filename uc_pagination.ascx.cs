ea3faf0bde3447c40c7ed4c09bcca4d7aa545e47
Bruno Mateus Casali <brunoocasali@gmail.com>
2014-08-12 09:57:05 -0300

    Translated to en-US

--- uc_pagination.ascx.cs
+++ uc_pagination.ascx.cs
@@ -9,87 +9,83 @@ public partial class UserControlPagination : System.Web.UI.UserControl
 
     protected void Page_Load(object sender, EventArgs e)
     {
-        //se não existe a chave 'p' na querystring adiciona ela.
+        //if in the page doesn't have the 'p' parameter on the query string add one
         if (!reg.IsMatch(Request.Url.PathAndQuery))
-        {
             Response.Redirect(Request.Url.PathAndQuery + "?p=1");
-        }
     }
 
     protected void MakePagination()
     {
         try
         {
-            #region Declara variáveis
-            string lnkAnterior = "", lnkProximo = "";
+            string lnkPrev = "", lnkNext = "";
             decimal pageCount = 1;
 
-            int pagAtual = (!string.IsNullOrEmpty(Request.QueryString["p"]) ? Convert.ToInt32(Request.QueryString["p"]) : 1);
-            #endregion
+            int actual = (!string.IsNullOrEmpty(Request.QueryString["p"]) ? Convert.ToInt32(Request.QueryString["p"]) : 1);
 
-            #region Valida Páginas
+            #region validate pages
             pageCount = Total != 0 ? Math.Ceiling(Convert.ToDecimal(Total) / Convert.ToDecimal(TotalPerPage)) : 1;
 
-            if (pagAtual == 1)
-                lnkAnterior = "javascript:;";
+            if (actual == 1)
+                lnkPrev = "javascript:;";
             else
-                lnkAnterior = mount(pagAtual - 1);
+                lnkPrev = mount(actual - 1);
 
-            if (pageCount == pagAtual)
-                lnkProximo = "javascript:;";
+            if (pageCount == actual)
+                lnkNext = "javascript:;";
             else
-                lnkProximo = mount(pagAtual + 1);
+                lnkNext = mount(actual + 1);
             #endregion
 
-            #region Paginação númerica
-            System.Text.StringBuilder sbPaginacao = new System.Text.StringBuilder();
-            int totalPagina = Convert.ToInt32(pageCount);
+            #region Numbering pagination
+            System.Text.StringBuilder sb = new System.Text.StringBuilder();
+            int total = Convert.ToInt32(pageCount);
 
-            int ini = pagAtual - 3,
+            int ini = actual - 3,
                 fim = 0;
 
-            if (pagAtual == 1)
-                fim = pagAtual + 4;
+            if (actual == 1)
+                fim = actual + 4;
 
-            else if (pagAtual == 2)
-                fim = pagAtual + 3;
+            else if (actual == 2)
+                fim = actual + 3;
 
-            else if (pagAtual == 3)
-                fim = pagAtual + 2;
+            else if (actual == 3)
+                fim = actual + 2;
 
-            else if (pagAtual > 3)
-                fim = pagAtual + 3;
+            else if (actual > 3)
+                fim = actual + 3;
 
             if (ini < 1)
                 ini = 1;
 
-            if (fim > totalPagina)
-                fim = totalPagina;
+            if (fim > total)
+                fim = total;
 
-            if (pagAtual >= totalPagina)
-                fim = pagAtual;
+            if (actual >= total)
+                fim = actual;
 
             while (ini < fim + 1)
             {
-                if (ini == pagAtual)
-                    sbPaginacao.AppendFormat(@"<a href=""javascript:;"" class=""paginaAtual"" title=""Página atual"">{0}</a>", ini);
+                if (ini == actual)
+                    sb.AppendFormat(@"<a href=""javascript:;"">{0}</a>", ini);
                 else
-                    sbPaginacao.AppendFormat(@"<a href=""{1}"" title=""Página {0}""> {0} </a>", ini, mount(ini));
+                    sb.AppendFormat(@"<a href=""{1}"" title=""Page {0}""> {0} </a>", ini, mount(ini));
                 ini++;
             }
             #endregion
 
-            #region Imprime na tela botões para paginação
-            Response.Write(string.Format(@"<p style=""color:#000;"">Página {2} de {3}</p>
-                                              <a href=""{0}"" title=""Página anterior"">&lt;&lt; Anterior</a>
+            #region Print buttons in the web page
+            Response.Write(string.Format(@"<p style=""color:#000;"">Page {2} of {3}</p>
+                                              <a href=""{0}"" title=""Previous page!"">&lt;&lt; Prev</a>
                                                 {4} 
-                                              <a href=""{1}"" title=""Proxima página"">Proximo &gt;&gt;</a>",
-                                            lnkAnterior, lnkProximo, pagAtual, pageCount, sbPaginacao.ToString()));
+                                              <a href=""{1}"" title=""Next page!"">Next &gt;&gt;</a>",
+                                            lnkPrev, lnkNext, actual, pageCount, sb.ToString()));
             #endregion
         }
         catch (Exception ex)
         {
-            Response.Write("Não foi possível montar a paginação ERRO: " + ex.Message);
+            Response.Write("You have some erros, please verify and correct them!: ERROR MESSAGE:" + ex.Message);
         }
     }
 
